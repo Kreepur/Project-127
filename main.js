ca39aae68b1683403dc0380f0f5e480757ecba3c
Kreepur <131872735+Kreepur@users.noreply.github.com>
2023-06-01 17:50:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Song1 = "";
+Song2 = ""; 
+
+function preload()
+{
+    Song1 = loadSound("music.mp3");
+    Song2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600 , 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video , 0 , 0 , 600 , 500);
+}
